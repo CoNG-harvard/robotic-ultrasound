624efb5eb19c27de2954f602f3edc4c796b89239
haitongma <haitongma@g.harvard.edu>
2023-08-23 22:29:57 -0400

    minor changes

--- sk/robo_main.py
+++ sk/robo_main.py
@@ -9,9 +9,9 @@ import torch.backends.cudnn as cudnn
 import sys
 os.environ['CUDA_VISIBLE_DEVICES']='0'
 sys.path.append("..")
+sys.path.append('/home/mht/catkin_ws/src/robotic-ultrasound')
 from segment_anything import sam_model_registry, SamPredictor
 import matplotlib.pyplot as plt
-from socket_helper import *
 import cv2
 
 def show_mask(mask, ax, random_color=False):
@@ -40,34 +40,18 @@ def load_data(filename):
     return data
 
 def run():
-    cudnn.benchmark = True
-    
-    """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-    """""""""""""""""""""""""""""""""""""""INERENCE"""""""""""""""""""""""""""""""""""""""
-    # socket_helper = Socket_Hepler_Host()
-    
-    # while True:
-    #     try: 
-    #         data = socket_helper.activate_server()
-    #         if data is not None:
-    #             break
-    #         else:
-    #             pass
-    #     except KeyboardInterrupt:
-    #         break
-        
-    data = load_data('/mount/home/local/PARTNERS/sk1064/workspace/control/data_retrieve/data_0.pkl')   
+    cudnn.benchmark = True        
+    data = load_data('/home/mht/temp/data_0.pkl')   
      
     with torch.no_grad():             
         with torch.cuda.amp.autocast():
-            sam_checkpoint = "/mount/home/local/PARTNERS/sk1064/workspace/control/sam_t/segment_anything/model/ckpt/sam_vit_h_4b8939.pth"
-            model_type = "vit_h"
+            sam_checkpoint = "/home/mht/catkin_ws/src/robotic-ultrasound/data/sam_vit_b_01ec64.pth"
+            model_type = "vit_b"
             
             device = "cuda"
 
             sam = sam_model_registry[model_type]( checkpoint=sam_checkpoint)
             sam.to(device=device)
-            
             print ("LOAD DONE")
             
     predictor = SamPredictor(sam)
@@ -78,7 +62,6 @@ def run():
     sh = np.shape(img)
     
     input_point = np.array([sh[0]//2, sh[1]//2])
-    
     input_point = np.array([[500, 375], [350, 625]])
     input_label = np.array([1, 0])
     
@@ -102,22 +85,7 @@ def run():
     show_mask(masks, plt.gca())
     plt.axis('off')
     plt.show() 
-    plt.savefig('test_.jpg')
-    
-    ###############################
-    ##### METHOD OPTIMIZATION #####
-    ###############################
-    ###############################
-    
-    socket_helper = Socket_Hepler_Host()
-    socket_helper.activate_server()
-    
-    while True:
-        try: 
-            socket_helper.Transfer_action(np.random())
-            pass
-        except KeyboardInterrupt:
-            break
+    plt.savefig('test_.jpg')   
                             
 if __name__ == '__main__':
     run()
