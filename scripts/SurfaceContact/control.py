5bac7ebd84dcb03c91a15dad6a039c6de68f9fe5
Tianpeng Zhang <44082254+mahaitongdae@users.noreply.github.com>
2023-10-17 19:22:49 -0400

    First successful surface contact recorded.

--- scripts/SurfaceContact/control.py
+++ scripts/SurfaceContact/control.py
@@ -4,16 +4,21 @@ from numpy import linalg as la
 from .scene import extractBodyPixels,bodyCentroid
 
 class SurfaceContactControl:
-    def __init__(self,rsPipeline):
+    def __init__(self,rsPipeline,rtde_c,rtde_r):
         self.pipeline = rsPipeline
+        self.rc = rtde_c
+        self.rr = rtde_r
         
         self.rgb_img = []
         self.mask = []
         self.target_loc = []
         self.center = []
-        self.offset = []
+        self.pixel_offset = []
 
-
+    def getContact(self):
+        speed = [0, 0, -0.050, 0, 0, 0]
+        self.rc.moveUntilContact(speed)
+    
     def mainloop(self):
         frames = self.pipeline.wait_for_frames()
         rgb = frames.get_color_frame()
@@ -22,9 +27,27 @@ class SurfaceContactControl:
         self.target_loc = np.array(bodyCentroid(self.mask),dtype = np.int)
 
         h,w = self.mask.shape
-        self.center = np.array([w//2,h//2])
-        self.offset = self.target_loc-self.center
-        return self.offset
+        # self.center = np.array([w//2,h//2])
+        self.center = np.array([w//2,280]) 
+        # The center of the probe is not the same as center of the image. Can be calibrated later.
+
+        self.pixel_offset = self.target_loc-self.center
+        
+        pixel_dist = la.norm(self.pixel_offset)
+        
+
+        step_size = min(pixel_dist/1000,0.05)
+        step = self.pixel_offset/pixel_dist * step_size
+        
+        q = self.rr.getActualTCPPose()
+        target_pose = np.array(q)
+        target_pose = self.rc.poseTrans(q,[step[0],step[1],0,
+                                                       0,0,0])
+        speed = 0.03
+        acc = 0.1
+        self.rc.moveL(target_pose,speed,acc)
+
+        return pixel_dist
 
 
     def showScene(self,axes):
@@ -43,9 +66,10 @@ class SurfaceContactControl:
         arrow_width = 5
         head_length = 4.5*arrow_width
         axes[1].arrow(*self.center,
-                *(self.offset-head_length*self.offset/la.norm(self.offset)),
+                *(self.pixel_offset-head_length*self.pixel_offset/la.norm(self.pixel_offset)),
                 color = 'red',width = arrow_width,head_length = head_length,
                 label = 'Moving direction')
         
         axes[1].imshow(result)
         axes[1].legend()
+        axes[1].set_title("Pixel distance to target:{}".format(la.norm(self.pixel_offset)))
--- scripts/notebooks/streamSeg.py
+++ scripts/notebooks/streamSeg.py
@@ -1,31 +1,49 @@
 import pyrealsense2 as rs
 import numpy as np
 import sys
+import cv2
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+
+import time
+import rtde_control
+import rtde_receive
 
 
 sys.path.append("../")
 
 from SurfaceContact.control import SurfaceContactControl
 
+from SurfaceContact.move import move_default_pose
+
 try:
     pipeline = rs.pipeline()
     pipeline.start()
-    sc = SurfaceContactControl(pipeline)
 
-    fig = plt.figure(dpi=100,figsize = (12,4))
-    ax1 = fig.add_subplot(1,2,1)
-    ax2 = fig.add_subplot(1,2,2)
+    rtde_c = rtde_control.RTDEControlInterface("192.168.1.13")
+    rtde_r = rtde_receive.RTDEReceiveInterface("192.168.1.13")
+    move_default_pose(rtde_c)
+    s = input("Start:?")
+    if s=='y':
+        sc = SurfaceContactControl(pipeline,rtde_c,rtde_r)
 
-    def animate(i):
-        ax1.clear()
-        ax2.clear()
-        sc.mainloop()
-        sc.showScene([ax1,ax2])
+        fig = plt.figure(dpi=100,figsize = (12,4))
+        ax1 = fig.add_subplot(1,2,1)
+        ax2 = fig.add_subplot(1,2,2)
+
+        for _ in range(40):
+            ax1.clear()
+            ax2.clear()
+            dist = sc.mainloop()
+            dist_threshold = 10
+            if dist<dist_threshold:
+                break
+            sc.showScene([ax1,ax2])
+            plt.pause(0.005)
+        print("Arrived at above target location.")
+
+        if sc.getContact():
+            print("Contact made.")
         
-
-    ani = animation.FuncAnimation(fig, animate,frames=40, interval=100)
-    plt.show()
+        
 finally:
     pipeline.stop()
