7f30992c54e30517f4a1e5c804b29b0843999438
Tianpeng Zhang <53916793+Tianpeng-Zhang@users.noreply.github.com>
2023-12-04 15:26:43 -0500

    Clean noisy pixels from recorded 3D

--- scripts/USImageCV/utils/bunny.py
+++ scripts/USImageCV/utils/bunny.py
@@ -6,12 +6,33 @@ import torch.nn.functional as F
 from ..models.unet import UNet
 from skimage.measure import label, regionprops
 
-def max_connected_area(mask):
-    label_im = label(mask)
-    regions = regionprops(label_im)
-    if len(regions)==0:
-        return 0
-    return max([r.area for r in regions])
+def max_connected_area(mask,return_mask=False):
+
+    labeled_mask = label(mask)
+    regions = regionprops(labeled_mask)
+    
+    max_area = 0 if len(regions)==0 else np.max([r.area for r in regions])
+
+    if not return_mask:
+        return max_area
+
+    else:
+
+        ndim = len(mask.shape)
+        out_mask = np.zeros(mask.shape)
+
+        if len(regions)>0:
+            max_region_idx = np.argmax([r.area for r in regions])
+            max_region = regions[max_region_idx]
+
+            if ndim == 2:
+                for i,j in max_region.coords:
+                    out_mask[i,j] = 1
+            elif ndim == 3:
+                for i,j,k in max_region.coords:
+                    out_mask[i,j,k] = 1
+        
+        return max_area,out_mask
 
 def load_bunny_model(ckpt_path,device):
     model = UNet(2,device,enable_classification=False)
